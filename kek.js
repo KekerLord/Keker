d9fff07859222a5273bcda3d69df49ef152f6f8e
noisyboy25 <dj402and@gmail.com>
2017-07-13 00:43:37 +0300

    Fixed some bugs

--- kek.js
+++ kek.js
@@ -7,41 +7,44 @@ const prefix = '.';
 function Spam() {
     client.guilds.first().members.find('id', '107544010687545344').send('Normal?'); //Andrey
     client.guilds.first().members.find('id', '108560701366210560').send('Dopili menya!'); //Me
-};
+    console.log('>spam');
+}
+
+function Ava(msg) {
+    msg.reply(msg.author.avatarURL);
+    console.log('>ava');
+}
+
+function Samokat(msg) {
+    msg.channel.send('https://www.youtube.com/watch?v=qQ1H9WtratY');
+    console.log('>samokat');
+}
+
+function Mops(msg) {
+    msg.channel.send('http://dogcatfan.com/uploads/posts/2016-11/1480462125_pug.jpg');
+    console.log('>mops');
+}
 
 client.on('ready', () => {
     console.log('Dorou');
 });
 
-client.login(config.token);
-
 client.on('message', (message) => {
     if (message.author.bot) return;
     switch (true) {
         case message.content.startsWith(prefix+'spam'):
-        Spam();
-        console.log('>spam');
-        break; 
+            Spam();
+            break;
+        case message.content.startsWith(prefix+'ava'):
+            Ava(message);
+            break;
+        case message.content.startsWith(prefix+'samokat'):
+            Samokat(message);
+            break;
+        case message.content.startsWith(prefix+'mops'):
+            Mops(message);
+            break;
     }
 });
 
-client.on('message', message => {
-    if (message.content.startsWith(prefix+'ava')) {
-        message.reply(message.author.avatarURL);
-        console.log('>ava');
-    }
-});
-
-client.on('message', message => {
-    if (message.content.startsWith(prefix+'mops')) {
-        message.channel.send('http://dogcatfan.com/uploads/posts/2016-11/1480462125_pug.jpg');
-       console.log('>mops');
-    }
-});
-
-client.on('message', message => {
-    if (message.content.startsWith(prefix+'samokat')) {
-        message.channel.send('https://www.youtube.com/watch?v=qQ1H9WtratY');
-        console.log('>samokat')
-    }
-});
+client.login(config.token);
