fdee57357c3baea0b6d66419b4cfcedaf2a44126
KekerLord <lou222@mail.ru>
2017-07-13 22:28:32 +0300

    Added functions .mops, .ava, .samokat

--- kek.js
+++ kek.js
@@ -7,6 +7,22 @@ const prefix = '.';
 function Spam() {
     client.guilds.first().members.find('id', '107544010687545344').send('Normal?'); //Andrey
     client.guilds.first().members.find('id', '108560701366210560').send('Dopili menya!'); //Me
+    console.log('>spam');
+};
+
+function Ava(message) {
+    message.reply(message.author.avatarURL);
+    console.log ('>ava');
+};
+
+function Samokat(message) {
+    message.channel.send('https://www.youtube.com/watch?v=qQ1H9WtratY');
+    console.log('>samokat');
+};
+
+function Mops(message) {
+    message.channel.send('http://dogcatfan.com/uploads/posts/2016-11/1480462125_pug.jpg');
+    console.log('>mops');
 };
 
 client.on('ready', () => {
@@ -20,28 +36,15 @@ client.on('message', (message) => {
     switch (true) {
         case message.content.startsWith(prefix+'spam'):
         Spam();
-        console.log('>spam');
         break; 
-    }
-});
-
-client.on('message', message => {
-    if (message.content.startsWith(prefix+'ava')) {
-        message.reply(message.author.avatarURL);
-        console.log('>ava');
-    }
-});
-
-client.on('message', message => {
-    if (message.content.startsWith(prefix+'mops')) {
-        message.channel.send('http://dogcatfan.com/uploads/posts/2016-11/1480462125_pug.jpg');
-       console.log('>mops');
-    }
-});
-
-client.on('message', message => {
-    if (message.content.startsWith(prefix+'samokat')) {
-        message.channel.send('https://www.youtube.com/watch?v=qQ1H9WtratY');
-        console.log('>samokat')
+        case message.content.startsWith(prefix+'ava'):
+        Ava(message);
+        break;
+        case message.content.startsWith(prefix+'samokat'):
+        Samokat(message);
+        break;
+        case message.content.startsWith(prefix+'mops'):
+        Mops(message);
+        break;
     }
 });
